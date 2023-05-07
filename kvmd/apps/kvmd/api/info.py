814a197481586ca2a6d3327106f13b94ba330451
david.chen01 <912865030@qq.com>
2023-05-07 20:09:20 +0800

    feature: 新增机箱、节点、网络功能

--- kvmd/apps/kvmd/api/info.py
+++ kvmd/apps/kvmd/api/info.py
@@ -21,6 +21,8 @@
 
 
 import asyncio
+import os
+import subprocess
 
 from typing import List
 
@@ -33,6 +35,8 @@ from ....htserver import make_json_response
 from ....validators.kvm import valid_info_fields
 
 from ..info import InfoManager
+
+
 
 
 # =====
@@ -50,6 +54,167 @@ class InfoApi:
             for field in fields
         ])))
         return make_json_response(results)
+    
+    @exposed_http("GET", "/node_status")
+    async def __common_get_nodestatus(self, request: Request) -> Response:
+        status, data = subprocess.getstatusoutput('nodstatus')
+        node_data_list = data.split('\n')
+        res = {}
+        for node_data in node_data_list:
+            key = node_data.split(' ')[1]
+            if key not in res:
+                res[key] = self.__parse_node_status(node_data)
+        return make_json_response(res)
+
+    def __parse_node_status(self, line):
+        if "OFF" in line:
+            return "OFF"
+        elif "ON" in line:
+            return "ON"
+        else:
+            return "N/A" 
+
+    @exposed_http("GET", "/netstat")
+    async def __common_get_netstat(self, request: Request) -> Response:
+        status, data = subprocess.getstatusoutput('sudo netcfg')
+        if status == 0:
+            res = self.__parse_netcfg(data)
+        else:
+            res = {'error': str(data)}
+        return make_json_response(
+            res
+        )
+    
+    @exposed_http("POST", "/netstat")
+    async def __common_set_netstat(self, request: Request) -> Response:
+        data = {}
+        try:
+            command = "sudo netcfg ip {}".format(request.query.get('address'))
+            status, res = subprocess.getstatusoutput(command)
+            command = "sudo netcfg mask {}".format(request.query.get('mask'))
+            status, res = subprocess.getstatusoutput(command)
+            command = "sudo netcfg gw {}".format(request.query.get('gateway'))
+            status, res = subprocess.getstatusoutput(command)
+            command = "sudo netcfg dns {}".format(request.query.get('dns'))
+            status, res = subprocess.getstatusoutput(command)
+            data['status'] = status
+            data['res'] = res
+        except Exception as e:
+            data['error'] = str(e)
+        return make_json_response(data)
+    
+    @exposed_http("POST", "/chassis_open")
+    async def __common_open_chassis(self, request: Request) -> Response:
+        data = {}
+        try:
+            command = "crpspwr on"
+            status, res = subprocess.getstatusoutput(command)
+            data['status'] = status
+            data['res'] = res
+        except Exception as e:
+            data['error'] = str(e)
+        return make_json_response(data)
+    
+    @exposed_http("POST", "/chassis_close")
+    async def __common_close_chassis(self, request: Request) -> Response:
+        data = {}
+        try:
+            command = "crpspwr off"
+            status, res = subprocess.getstatusoutput(command)
+            data['status'] = status
+            data['res'] = res
+        except Exception as e:
+            data['error'] = str(e)
+        return make_json_response(data)
+    
+    @exposed_http("GET", "/chassis_status")
+    async def __common_get_chassis_status(self, request: Request) -> Response:
+        data = {}
+        try:
+            command = "sudo crpspwr status"
+            status, res = subprocess.getstatusoutput(command)
+            data['chassis_status'] = 'ON' if 'ON' in res else 'OFF'
+            command = "sudo kvmstatus"
+            status, res = subprocess.getstatusoutput(command)
+            if status != 0:
+                data['error'] = res
+                data['kvm_status'] = 1
+            else:
+                data['kvm_status'] = res
+            data['status'] = status
+        except Exception as e:
+            data['error'] = str(e)
+
+        return make_json_response(data)
+    
+    @exposed_http("GET", "/chassis_type")
+    async def __common_get_node_num(self, request: Request) -> Response:
+        data = {}
+        if not os.path.exists('/etc/chassis_type'):
+            num = 4
+        with open('/etc/chassis_type', 'rb') as f:
+            res = f.read()
+            num = int(res.decode().split('u')[1])
+        data['num'] = num
+        data['res'] = self.__get_node_status()
+        return make_json_response(data)
+    
+    @exposed_http("POST", "/open_node")
+    async def __node_open(self, request: Request) -> Response:
+        data = {}
+        try:
+            num = request.query.get('num')
+            command = "sudo nodpwron {}".format(num)
+            status, res = subprocess.getstatusoutput(command)
+            data['status'] = status
+            data['res'] = res
+        except Exception as e:
+            data['error'] = str(e)
+        return make_json_response(data)
+    
+    @exposed_http("POST", "/close_node")
+    async def __node_close(self, request: Request) -> Response:
+        data = {}
+        try:
+            num = request.query.get('num')
+            command = "sudo nodpwroff {}".format(num)
+            status, res = subprocess.getstatusoutput(command)
+            data['status'] = status
+            data['res'] = res
+        except Exception as e:
+            data['error'] = str(e)
+        return make_json_response(data)
+    
+    def __exchange_maskint(self, mask_int):
+        bin_arr = ['0' for i in range(32)]
+        for i in range(mask_int):
+            bin_arr[i] = '1'
+        tmp_mask = [''.join(bin_arr[i*8: i*8 + 8]) for i in range(4)]
+        tmp_mask = [str(int(tmpstr, 2)) for tmpstr in tmp_mask]
+        return '.'.join(tmp_mask)
+
+    def __exchange_maskstr(self, mask_str):
+        return sum([bin(int(i)).count('1') for i in mask_str.split('.')])
+
+    def __parse_netcfg(self, data):
+        res = {
+            'Name': '',
+            'Address': '',
+            'Gateway': '',
+            'DNS': ''
+        }
+        mask = ''
+        items = data.split('\n')
+        for item in items:
+            for key in list(res.keys()):
+                if item.startswith(key):
+                    res[key] = item.split('=')[1]
+                    if key == "Address":
+                        value = res[key]
+                        res[key] = value.split('/')[0]
+                        mask = self.__exchange_maskint(int(value.split('/')[1]))
+        res['Mask'] = mask
+        return res
 
     def __valid_info_fields(self, request: Request) -> List[str]:
         subs = self.__info_manager.get_subs()
@@ -57,3 +222,20 @@ class InfoApi:
             arg=request.query.get("fields", ",".join(subs)),
             variants=subs,
         ) or subs)
+
+    def __get_node_status(self):
+        res = {}
+        command = "nodstatus"
+        status, data = subprocess.getstatusoutput(command)
+        if status == 0:
+            index = 1
+            temps = data.split('\n')
+            for temp in temps:
+                if 'ON' in temp:
+                    res[index] = 'ON'
+                elif 'OFF' in temp:
+                    res[index] = 'OFF'
+                else:
+                    res[index] = 'N/A'
+                index += 1
+        return res
--- web/share/js/kvm/recorder.js
+++ web/share/js/kvm/recorder.js
@@ -45,10 +45,29 @@ export function Recorder() {
 		tools.el.setOnClick($("hid-recorder-stop"), __stopProcess);
 		tools.el.setOnClick($("hid-recorder-play"), __playRecord);
 		tools.el.setOnClick($("hid-recorder-clear"), __clearRecord);
+		tools.el.setOnClick($("hid-recorder-netstat"), __setNetstat);
 
 		$("hid-recorder-new-script-file").onchange = __uploadScript;
 		tools.el.setOnClick($("hid-recorder-upload"), () => $("hid-recorder-new-script-file").click());
 		tools.el.setOnClick($("hid-recorder-download"), __downloadScript);
+		tools.el.setOnClick($("hid-node-open-1"), __open_node1);
+		tools.el.setOnClick($("hid-node-open-2"), __open_node2);
+		tools.el.setOnClick($("hid-node-open-3"), __open_node3);
+		tools.el.setOnClick($("hid-node-open-4"), __open_node4);
+		tools.el.setOnClick($("hid-node-open-5"), __open_node5);
+		tools.el.setOnClick($("hid-node-open-6"), __open_node6);
+		tools.el.setOnClick($("hid-node-open-7"), __open_node7);
+		tools.el.setOnClick($("hid-node-open-8"), __open_node8);
+		tools.el.setOnClick($("hid-node-close-1"), __close_node1);
+		tools.el.setOnClick($("hid-node-close-2"), __close_node2);
+		tools.el.setOnClick($("hid-node-close-3"), __close_node3);
+		tools.el.setOnClick($("hid-node-close-4"), __close_node4);
+		tools.el.setOnClick($("hid-node-close-5"), __close_node5);
+		tools.el.setOnClick($("hid-node-close-6"), __close_node6);
+		tools.el.setOnClick($("hid-node-close-7"), __close_node7);
+		tools.el.setOnClick($("hid-node-close-8"), __close_node8);
+		tools.el.setOnClick($("hid-chassis-open"), __open_chassis);
+		tools.el.setOnClick($("hid-chassis-close"), __close_chassis);
 	};
 
 	/************************************************************************/
@@ -113,6 +132,26 @@ export function Recorder() {
 		}
 		__refresh();
 	};
+
+	var __setNetstat = function() {
+		let address = $("hid-network-input-address").value
+		let gateway = $("hid-network-input-gateway").value
+		let dns = $("hid-network-input-dns").value
+		let mask = $("hid-network-input-mask").value
+		// let body = {
+		// 	"address": $("hid-network-input-address").value,
+		// 	"gateway": $("hid-network-input-gateway").value,
+		// 	"dns": $("hid-network-input-dns").value
+		// }
+		let url = "/api/netstat?address=" + address + "&gateway=" + gateway + "&dns=" + dns + "&mask=" + mask
+		let http = tools.makeRequest("POST", url, function() {
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+		});
+	}
 
 	var __playRecord = function() {
 		__play_timer = setTimeout(() => __runEvents(0), 0);
@@ -306,6 +345,9 @@ export function Recorder() {
 		tools.el.setEnabled($("hid-recorder-download"), (!__play_timer && !__recording && __events.length));
 
 		__setCounters(__events.length, __events_time);
+		__getNetConfig();
+		__getNodeStatus();
+		__get_chassis_status();
 	};
 
 	var __setCounters = function(events_count, events_time) {
@@ -313,5 +355,236 @@ export function Recorder() {
 		$("hid-recorder-events-count").innerHTML = events_count;
 	};
 
+	var __getNetConfig = function() {
+		let http = tools.makeRequest("GET", "/api/netstat", function() {
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+			console.log(http.responseText)
+			let info = JSON.parse(http.responseText).result;
+			$("hid-network-input-address").value = info.Address;
+			$("hid-network-input-gateway").value = info.Gateway;
+			$("hid-network-input-dns").value = info.DNS;
+			$("hid-network-input-mask").value = info.Mask;
+		});		
+	};
+
+	var __getNodeStatus = function() {
+		console.log("get node status");
+		let http = tools.makeRequest("GET", "/api/chassis_type", function() {
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+			let node_num = JSON.parse(http.responseText).result.num;
+			let res = JSON.parse(http.responseText).result.res;
+			console.log(node_num)
+			//var node_num = 2;
+			for (var i=1;i<=node_num;i++) {
+				// var open_temp_id = "hid-node-open-" + i.toString();
+				// tools.el.setEnabled($(open_temp_id), false);
+				// var close_temp_id = "hid-node-close-" + i.toString();
+				// tools.el.setEnabled($(close_temp_id), false);
+				var temp_id = "hid-node-" + i.toString();
+				tools.feature.setEnabled($(temp_id), true);
+				var temp_id1 = "hid-node" + i.toString();
+				$(temp_id1).innerHTML = res[i];
+				
+			}
+
+		});	
+		// var node_num = 2;
+		// for (var i=1;i<=node_num;i++) {
+		// 	// var open_temp_id = "hid-node-open-" + i.toString();
+		// 	// tools.el.setEnabled($(open_temp_id), false);
+		// 	// var close_temp_id = "hid-node-close-" + i.toString();
+		// 	// tools.el.setEnabled($(close_temp_id), false);
+		// 	var temp_id = "hid-node-" + i.toString();
+		// 	tools.feature.setEnabled($(temp_id), true);
+			
+		// }
+		// 获取节点数量，屏蔽未使用到的节点
+		// let node1_status = "not exist";
+		// let node2_status = "not exist";
+		// let node3_status = "OFF";
+		// let node4_status = "ON";
+		// $("hid-node1").innerHTML = node1_status;
+		// $("hid-node2").innerHTML = node2_status;
+		// $("hid-node3").innerHTML = node3_status;
+		// $("hid-node4").innerHTML = node4_status;
+		// if (node1_status == "not exist") {
+		// 	tools.el.setEnabled($("hid-node-open-1"), false);
+		// 	tools.el.setEnabled($("hid-node-close-1"), false);
+		// } else {
+		// 	tools.el.setEnabled($("hid-node-open-1"), true);
+		// 	tools.el.setEnabled($("hid-node-close-1"), true);
+		// }
+
+		// if (node2_status == "not exist") {
+		// 	tools.el.setEnabled($("hid-node-open-2"), false);
+		// 	tools.el.setEnabled($("hid-node-close-2"), false);
+		// } else {
+		// 	tools.el.setEnabled($("hid-node-open-2"), true);
+		// 	tools.el.setEnabled($("hid-node-close-2"), true);
+		// }
+
+		// if (node3_status == "not exist") {
+		// 	tools.el.setEnabled($("hid-node-open-3"), false);
+		// 	tools.el.setEnabled($("hid-node-close-3"), false);
+		// } else {
+		// 	tools.el.setEnabled($("hid-node-open-3"), true);
+		// 	tools.el.setEnabled($("hid-node-close-3"), true);
+		// }
+
+		// if (node4_status == "not exist") {
+		// 	tools.el.setEnabled($("hid-node-open-4"), false);
+		// 	tools.el.setEnabled($("hid-node-close-4"), false);
+		// } else {
+		// 	tools.el.setEnabled($("hid-node-open-4"), true);
+		// 	tools.el.setEnabled($("hid-node-close-4"), true);
+		// }
+
+	};
+
+	var __open_node = function(i) {
+		let url = "/api/open_node?num=" + i;
+		let http = tools.makeRequest("POST", url, function() {
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+		});
+	};
+
+	var __close_node = function(i) {
+		let url = "/api/close_node?num=" + i;
+		let http = tools.makeRequest("POST", url, function() {
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+		});
+	};
+
+	var __open_node1 = function() {
+		console.log("open node 1");
+		__open_node(1);
+	};
+
+	var __close_node1 = function() {
+		console.log("close node 1");
+		__close_node(1);
+	};
+
+	var __open_node2 = function() {
+		console.log("open node 2");
+		__open_node(2);
+	};
+
+	var __close_node2 = function() {
+		console.log("close node 2");
+		__close_node(2);
+	};
+
+	var __open_node3 = function() {
+		console.log("open node 3");
+		__open_node(3);
+	};
+
+	var __close_node3 = function() {
+		console.log("close node 3");
+		__close_node(3);
+	};
+
+	var __open_node4 = function() {
+		console.log("open node 4");
+		__open_node(4);
+	};
+
+	var __close_node4 = function() {
+		console.log("close node 4");
+		__close_node(4);
+	};
+
+	var __open_node5 = function() {
+		console.log("open node 5");
+		__open_node(5);
+	};
+
+	var __close_node5 = function() {
+		console.log("close node 5");
+		__close_node(5);
+	};
+
+	var __open_node6 = function() {
+		console.log("open node 6");
+		__open_node(6);
+	};
+
+	var __close_node6 = function() {
+		console.log("close node 6");
+		__close_node(6);
+	};
+
+	var __open_node7 = function() {
+		console.log("open node 7");
+		__open_node(7);
+	};
+
+	var __close_node7 = function() {
+		console.log("close node 7");
+		__close_node(7);
+	};
+
+	var __open_node8 = function() {
+		console.log("open node 8");
+		__open_node(8);
+	};
+
+	var __close_node8 = function() {
+		console.log("close node 8");
+		__close_node(8);
+	};
+
+	var __get_chassis_status = function() {
+		let http = tools.makeRequest("GET", "/api/chassis_status", function() {
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+			let res = JSON.parse(http.responseText).result;
+			$("hid-chassis-status").innerHTML = res.chassis_status;
+			$("hid-chassis-kvm-status").innerHTML = res.kvm_status;
+		});	
+		
+	};
+
+	var __open_chassis = function() {
+		let http = tools.makeRequest("POST", "/api/chassis_open", function() {
+			console.log("open chassis");
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+		});	
+	};
+
+	var __close_chassis = function() {
+		let http = tools.makeRequest("POST", "/api/chassis_close", function() {
+			console.log("close chassis");
+			if (http.readyState === 4) {
+				if (http.status !== 200) {
+					wm.error("HID reset error:<br>", http.responseText);
+				}
+			}
+		});	
+	};
 	__init__();
 }
