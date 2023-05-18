6049a56b5ca19c2766e5da38e370cb0bd1f790bb
david.chen01 <912865030@qq.com>
2023-05-18 21:17:37 +0800

    fix: 优化界面显示及脚本

--- scripts/patch_install.sh
+++ scripts/patch_install.sh
@@ -4,20 +4,25 @@
 #current_timestamp=$(date +"%s")
 current_timestamp=bak
 
-kvmd_path=/usr/lib/python3/dist-packages/kvmd/apps/kvmd/api
+kvm_root_path=/usr/lib/python3/dist-packages/kvmd/apps
+
+kvmd_path=$kvm_root_path/kvmd/api
 web_js_path=/usr/share/kvmd/web/share/js/kvm
 web_path=/usr/share/kvmd/web/kvm
+htpaswd_path=$kvm_root_path/htpasswd
 
 echo "补丁更新开始"
 echo "开始备份原有文件"
 cp -rv $kvmd_path/info.py $kvmd_path/info.py_$current_timestamp || exit -1
 cp -rv $web_js_path/recorder.js $web_js_path/recorder.js_$current_timestamp || exit -1
 cp -rv $web_path/index.html $web_path/index.html_$current_timestamp || exit -1
+cp -rv $htpaswd_path/__init__.py $htpaswd_path/__init__.py_$current_timestamp || exit -1
 
 echo "更新补丁文件到系统"
 cp -rv info.py $kvmd_path/ || exit -1
 cp -rv recorder.js $web_js_path/ || exit -1
 cp -rv index.html $web_path/ || exit -1
+cp -rv __init__.py $htpaswd_path/ || exit -1
 
 echo "重启kvmd服务"
 systemctl restart kvmd
